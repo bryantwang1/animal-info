c467079ba1dfed7bfd202fa432b9384396b1a82e
Bryant Wang and Kevin Finley <kevin@>
2016-10-20 08:25:46 -0700

    Add form to index.html, set form to send users to Wikipedia depending on selection.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(function() {
+$(document).ready(function() {
+  $("form#formMain").submit(function(event) {
+    event.preventDefault();
 
+    var whichAnimal = $("#animalsForm").val();
+
+    if(whichAnimal = "turtles") {
+      window.location.href = "https://en.wikipedia.org/wiki/Turtle";
+    } else if(whichAnimal = "snakes") {
+      window.location.href = "https://en.wikipedia.org/wiki/Snake";
+    } else {
+      window.location.href = "https://en.wikipedia.org/wiki/Insect";
+    }
+  });
 });
